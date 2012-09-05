59637b77825415391fec3b9f6366b09ead3a3856
Erric <erricgunawan@gmail.com>
2012-09-05 11:23:59 +0700

    initial commit

---
+++ erricblue.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Plugin Name: Erric Blue
+ * Plugin URI: http://www.wpbeginner.com/beginners-guide/what-why-and-how-tos-of-creating-a-site-specific-wordpress-plugin/
+ * Description: Site specific code changes for Eric Gunawan in Blue
+ * Version: 0.1
+ * Author: erricgunawan
+ * Author URI: http://erricgunawan.com
+
+/* Start Adding Functions Below this Line */
+
+
+
+/**
+ * Enqueue styles
+ */
+function erric_styles() {
+	wp_enqueue_style('erricblue', plugins_url( basename( plugin_dir_path(__FILE__) ), basename( __FILE__ ) ) . '/css/erricblue.css');
+}
+add_action('wp_print_styles', 'erric_styles');
+
+/**
+ * Enqueue scripts
+ *
+function erric_scripts() {
+	wp_enqueue_script('erricblue-js', plugins_url( basename( plugin_dir_path(__FILE__) ), basename( __FILE__ ) ) . '/js/erricblue.js', array('jquery'));
+}
+add_action('wp_print_scripts', 'erric_scripts'); */
+
+
+
+/**
+ * Extending The Recent Comments Widget
+ * http://www.ghostpool.com/freebies/creating-a-recent-comments-widget/
+ */
+add_action( 'widgets_init', 'erric_widgets_init' );
+
+function erric_widgets_init() {
+	register_widget('Recent_Comments');
+}
+
+class Recent_Comments extends WP_Widget {
+	
+	function Recent_Comments() {
+        $widget_ops = array('classname' => 'recent_comments', 'description' => __('Displays recent comments with avatars and comment excerpts.', 'erric'));
+        $this->WP_Widget('recent-comments', __('Recent Comments', 'erric'), $widget_ops);
+    }
+	
+	function widget($args, $instance) {
+		extract($args);
+		$title = apply_filters('widget_title', $instance['title']);
+		$number = $instance['number'];
+
+		// Begin Widget
+		echo $before_widget;
+
+		if ($title)
+			echo $before_title . $title . $after_title;
+		?>
+			
+		<div id="recent-comments">
+			
+		    <ul>
+				
+				<?php
+				global $wpdb;
+				$request = "SELECT * FROM $wpdb->comments";
+				$request .= " JOIN $wpdb->posts ON ID = comment_post_ID";
+				$request .= " WHERE comment_approved = '1' AND post_status = 'publish' AND post_password =''";
+				$request .= " AND user_id != 1 AND comment_type != 'pingback'";
+				$request .= " ORDER BY comment_date DESC LIMIT $number";
+				$comments = $wpdb->get_results($request);
+				if ($comments) {
+					foreach ($comments as $comment) {
+						ob_start();
+				?>
+							
+					<li>
+
+						<?php echo get_avatar($comment, $size = '40'); ?>
+
+						<div class="comment-excerpt">
+							<a href="<?php echo get_permalink($comment->comment_post_ID) . '#comment-' . $comment->comment_ID; ?>"><?php echo strip_tags(substr(apply_filters('get_comment_text', $comment->comment_content), 0, 30)); ?>...</a>
+							<br/><span class="comment-author"><?php echo get_comment_author_link($comment->comment_ID); ?> <?php _e('on'); ?> <?php echo get_the_title($comment->comment_post_ID) ?></span>
+						</div>
+
+					</li>
+								
+				<?php ob_end_flush();
+					}
+				} else { // If no comments  ?>
+						
+					<li><?php _e('No comments.', 'erric'); ?></li>
+							
+			<?php } ?>
+					
+		    </ul>
+				
+		</div>
+			
+		<?php
+		echo $after_widget;
+		// End Widget
+	}
+ 
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['number'] = strip_tags($new_instance['number']);
+		return $instance;
+	}
+ 
+	function form( $instance ) {
+		$defaults = array('title' => __('Recent Comments', 'erric'), 'number' => '5'); $instance = wp_parse_args((array) $instance, $defaults ); ?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+			<br/><input class="widefat" type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $instance['title']; ?>" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of Comments:', 'erric'); ?></label>
+			<input type="text" id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" size="3" value="<?php echo $instance['number']; ?>" />
+		</p>
+
+		<?php
+	}
+	
+}
+
+
+
+/**
+ * Change Email Headers 
+ * http://www.wprecipes.com/change-wordpress-from-email-header
+ */
+function erric_fromemail($email) {
+    $wpfrom = get_option('admin_email');
+    return $wpfrom;
+}
+ 
+function erric_fromname($email){
+    $wpfrom = get_option('blogname');
+    return $wpfrom;
+}
+
+add_filter('wp_mail_from', 'erric_fromemail');
+add_filter('wp_mail_from_name', 'erric_fromname');
+
+/* Stop Adding Functions Below this Line */
